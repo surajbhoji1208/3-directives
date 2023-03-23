aef53f11d43cc0a15d65ecb578b0fb74f68ea27e
surajbhoji1208 <60956130+surajbhoji1208@users.noreply.github.com>
2023-03-23 09:44:22 +0530

    Add files via upload

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,31 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = 'directives';
-  player = [
-    { id: 1, name: "rohit sharma", runs: 60 },
-    { id: 2, name: "virat Kohli", runs: 100 },
-    { id: 3, name: "suryakumar Yadav", runs: 100 },
-  ]
-
-
-  constructor() {
-    console.log()
-  }
-  totalRun = 0
-  count = this.player
-  ngOnInit(): void {
-    for (let i = 0; i < this.player.length; i++) {
-      this.totalRun += this.player[i].runs
-
-    }
-
-  }
-
-
+export class AppComponent {
+  title = 'decorative';
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductComponent } from './product/product.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import { InputParentComponent } from './InputDecorative/input-parent/input-parent.component';
+import { InputChildComponent } from './InputDecorative/InputParent/input-child/input-child.component';
+import { OutputParentComponent } from './OutputDecorative/output-parent/output-parent.component';
+import { OutputchildComponent } from './OutputDecorative/OutputParent/outputchild/outputchild.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent
+    InputParentComponent,
+    InputChildComponent,
+    OutputParentComponent,
+    OutputchildComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
